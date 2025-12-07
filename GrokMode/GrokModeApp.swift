8b33c0309037bdd5005870b8f847a6947d6342a6
Aziz Albahar <abdulaziz@albahar.net>
2025-12-06 17:32:09 -0800

    Initial Commit

---
+++ GrokMode/GrokModeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrokModeApp.swift
+//  GrokMode
+//
+//  Created by Abdulaziz Albahar on 12/6/25.
+//
+
+import SwiftUI
+
+@main
+struct GrokModeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GrokModeTests/GrokModeTests.swift
@@ -0,0 +1,17 @@
+//
+//  GrokModeTests.swift
+//  GrokModeTests
+//
+//  Created by Abdulaziz Albahar on 12/6/25.
+//
+
+import Testing
+@testable import GrokMode
+
+struct GrokModeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
