9617eea5de7b5dbd2f91ac7aab270272d1641f92
Aziz Albahar <abdulaziz@albahar.net>
2026-01-01 17:35:21 -0600

    shaking + snapshot

---
+++ iOS/GrokMode/Extensions/Shaking+Ext.swift
@@ -0,0 +1,25 @@
+//
+//  Shaking+Ext.swift
+//  GrokMode
+//
+//  Created by Abdulaziz Albahar on 1/1/26.
+//
+
+import Foundation
+import UIKit
+
+// Source - https://stackoverflow.com/a
+// Posted by markiv
+// Retrieved 2026-01-01, License - CC BY-SA 4.0
+
+extension Notification.Name {
+    public static let deviceDidShakeNotification = Notification.Name("MyDeviceDidShakeNotification")
+}
+
+extension UIWindow {
+    open override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        super.motionEnded(motion, with: event)
+        NotificationCenter.default.post(name: .deviceDidShakeNotification, object: event)
+    }
+}
+
